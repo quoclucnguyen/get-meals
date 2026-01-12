ab05711a04f1568789f5ebae6ce0f71502c619b7
Luke Nguyen <nguyenquocluc@wavenet.com.tw>
2026-01-12 07:01:34 +0000

    Update cron job frequency to every 10 minutes and adjust logging message accordingly

--- src/tasks.service.ts
+++ src/tasks.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
-  @Cron('*/14 * * * *')
+  @Cron(CronExpression.EVERY_10_MINUTES)
   handleCron() {
-    this.logger.debug('Called every 14 minutes');
+    this.logger.debug('Called every 10 minutes');
   }
 }
